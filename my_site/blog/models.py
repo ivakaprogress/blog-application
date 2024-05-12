e2c64d645404adeef636e5a92d783edbccc128e1
ivakaprogress <ivaylotodorov93@gmail.com>
2024-05-12 21:35:30 +0300

    push to main

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -9,16 +17,25 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    create = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length = 2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
-        indexes = [models.Index(fields=['-publish'])]
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
